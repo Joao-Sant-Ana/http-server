c0f9bb1cbdb16aabf4086e48865c07330a0e3673
Joao Sant Ana <jmpsantana15@gmail.com>
2025-07-03 16:19:30 -0300

    Feat: Maded it responds with 200

--- main.go
+++ main.go
@@ -1,9 +1,42 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+	"time"
 )
 
 func main() {
-	fmt.Print("Hello world")
+	log.Println("Hello world")
+	ln, err := net.Listen("tcp", "0.0.0.0:8000")
+	if err != nil {
+		log.Fatalf("Error: %v\n", err)
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("Error: %v\n", err)
+			break
+		}
+
+		handleConn(conn)
+	}
+
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+	r := make([]byte, 256)
+	c.SetReadDeadline(time.Now().Add(5 * time.Second))
+	_, err := c.Read(r)
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
+	
+	response := "HTTP/1.1 200 OK\r\n\r\n"
+	_, err = c.Write([]byte(response))
+	if err != nil {
+		log.Printf("Error: %v\n", err)
+	}
 }
